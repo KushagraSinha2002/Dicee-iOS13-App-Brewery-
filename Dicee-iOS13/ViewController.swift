64b4d538bfb8ad10b2278d8f01b93a5f873ceb28
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-06-11 04:21:34 +0530

    Randomised the dices when clicked Roll

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -17,28 +17,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Who.What =
+//         Who.What =
         
-        //Two ways to change a IBoutlet image to another image when the app launches
+//        Two ways to change a IBoutlet image to another image when the app launches
         
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
-        // diceImageView1.image = #imageLiteral(
-        // OR
-        //diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+//        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+//         diceImageView1.image = #imageLiteral(
+//         OR
+//        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
         
         // change opacity of image
-        //diceImageView1.alpha = 0.5
+//        diceImageView1.alpha = 0.5
         
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
         
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Roll button Pressed")
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
+//        print("Roll button Pressed")
+//        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
+//        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
+        
+        let diceImages = [ UIImage(named: "DiceOne"),
+                           UIImage(named: "DiceTwo"),
+                           UIImage(named: "DiceThree"),
+                           UIImage(named: "DiceFour"),
+                           UIImage(named: "DiceFive"),
+                           UIImage(named: "DiceSix") ]
+        
+        diceImageView1.image = diceImages.randomElement()!
+        diceImageView2.image = diceImages.randomElement()!
+        
     }
-    //test commit
 
 }
-
